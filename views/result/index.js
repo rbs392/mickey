27735a5413491e7163c1fbe56a280ee0fa3eaad4
rbs392 <rbs392@yahoo.com>
2017-01-16 14:33:36 +0530

    using listview for showing result

--- views/result/index.js
+++ views/result/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {
   View,
+  ListView,
   Text,
   Image,
   Button,
@@ -12,6 +13,10 @@ import style from './styles'
 export default class Result extends Component {
   constructor(props) {
     super(props)
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      data: ds.cloneWithRows(props.data)
+    };
     this.gotoSearch = this.gotoSearch.bind(this)
   }
   gotoSearch() {
@@ -20,22 +25,22 @@ export default class Result extends Component {
   render() {
     return (
       <View style={style.container}>
-      {
-        this.props.data.map(x => (
-          <View key={x.id} style={style.result}>
-            <Image
-              source={{uri: `data:image/png;base64,${x.metadata.rawImage}`}}
-              style={style.image}
-            />
-            <View>
-              <Text>{x.metadata.title}</Text>
-              <Text>{x.score}</Text>
+        <ListView
+          dataSource={this.state.data}
+          renderRow={(x) => (
+            <View key={x.id} style={style.result}>
+              <Image
+                source={{uri: `data:image/png;base64,${x.metadata.rawImage}`}}
+                style={style.image}
+              />
+              <View>
+                <Text>{x.metadata.title}</Text>
+                <Text>{x.score}</Text>
+              </View>
             </View>
-          </View>
-
-        ))
-      }
-      <Button onPress={this.gotoSearch} title="Back to search" />
+          )}
+        />
+        <Button onPress={this.gotoSearch} title="Back to search" />
       </View>
     )
   }
